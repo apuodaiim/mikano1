fab7e7fc5b0576d6a46be5ef0300e4263450b5c9
apuodaiim <apuodaiim@gmail.com>
2025-05-22 10:07:59 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,24 @@
+const cacheName = "data-entry-cache-v1";
+const filesToCache = [
+  "index.html",
+  "style.css",
+  "script.js",
+  "icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
